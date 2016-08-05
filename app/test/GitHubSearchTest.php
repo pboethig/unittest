fcd63632c3ca447430f1db65b0472d058d3082b5
pboethig <pboethig@gmail.com>
2016-08-05 12:45:40 +0200

    add more complex seleiumtest

---
+++ app/test/GitHubSearchTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 05.08.16
+ * Time: 10:58
+ */
+
+class GitHubSearchTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var RemoteWebDriver
+     */
+    protected $_webdriver;
+
+
+    protected $_url = 'https://github.com';
+
+    public function setUp()
+    {
+        $capabilities = [
+            \WebDriverCapabilityType::BROWSER_NAME=>'chrome',
+            \WebDriverCapabilityType::BROWSER_NAME=>'firefox'
+        ];
+
+        $this->_webdriver = RemoteWebDriver::create('http://localhost:4443/wd/hub', $capabilities);
+    }
+
+    public function GitHub()
+    {
+        //get content of an url
+        $this->_webdriver->get($this->_url);
+
+        //check for word "GitHub"
+        $this->assertContains('GitHub', $this->_webdriver->getTitle());
+    }
+
+
+    public function testSearch()
+    {
+        $term = 'vagrant_puppet_magento';
+
+        //get url
+        $this->_webdriver->get($this->_url);
+
+        //find the searchfield
+        $search = $this->_webdriver->findElement(WebDriverBy::cssSelector('.header-search-input'));
+
+        //set focus on this field
+        $search->click();
+
+        //type in searchstring and submit the form
+        $search->sendKeys($term)->submit();
+
+        $searchUrl = $this->_url . "/search?utf8=%E2%9C%93&q=".$term;
+
+        $this->_webdriver->get($searchUrl);
+
+        $content = $this->_webdriver->findElement(WebDriverBy::tagName('body'))->getText();
+
+        $this->assertContains($term, $content);
+    }
+
+
+    public function tearDown()
+    {
+        $this->_webdriver->close();
+        $this->_webdriver->quit();
+    }
+}
